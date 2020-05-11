c2c504d6322d4f54718ec5bc7cb007813c8e0975
ayub <yuanaayubs@gmail.com>
2020-05-11 22:01:19 +0700

    customing header

--- app/src/main/java/com/zub/covid_19/MainActivity.java
+++ app/src/main/java/com/zub/covid_19/MainActivity.java
@@ -1,10 +1,9 @@
 package com.zub.covid_19;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -13,41 +12,49 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
     private String linkPerProvinsi = "https://services5.arcgis.com/VS6HdKS0VfIhv8Ct/arcgis/rest/services/COVID19_Indonesia_per_Provinsi/FeatureServer/0/query?f=json&where=(Kasus_Posi%20%3C%3E%200)%20AND%20(Provinsi%20%3C%3E%20%27Indonesia%27)&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=Kasus_Posi%20desc&resultOffset=0&resultRecordCount=34&resultType=standard&cacheHint=true";
     private String linkTest = "http://echo.jsontest.com/insert-key-here/insert-value-here/key/value";
-//    TextView mTextView = findViewById(R.id.textView);
+    private JSONObject responses;
+    private boolean isFinishFetch = false;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-
-//        mTextView.setText("a;lkdfj");
+    private void dataResponse(String url){
 
         RequestQueue requestQueue = Volley.newRequestQueue(this);
-
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
                 Request.Method.GET,
-                linkTest,
+                url,
                 null,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
-//                        mTextView.setText("response.toString()");
-                        Log.e("Response: ", response.toString());
+                        isFinishFetch = true;
+                        responses = response;
                     }
                 },
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-//                        mTextView.setText(error.toString());
-                        Log.e("Response: ", error.toString());
+                        Log.e("Error Volley: ", error.toString());
                     }
                 }
         );
+
         requestQueue.add(jsonObjectRequest);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        dataResponse(linkPerProvinsi);
+        if(isFinishFetch){
+            Log.d("Resp", String.valueOf(responses));
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }

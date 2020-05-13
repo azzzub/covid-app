7546036d9588f0b04cb6a10cf012b96a582df35f
ayub <yuanaayubs@gmail.com>
2020-05-13 16:03:53 +0700

    adding bottom nav

---
+++ app/src/main/java/com/zub/covid_19/InfoFragment.java
@@ -0,0 +1,18 @@
+package com.zub.covid_19;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class InfoFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_info, container, false);
+    }
+}
--- app/src/main/java/com/zub/covid_19/MainActivity.java
+++ app/src/main/java/com/zub/covid_19/MainActivity.java
@@ -2,8 +2,11 @@ package com.zub.covid_19;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -11,51 +14,44 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
-
-    private String linkPerProvinsi = "https://services5.arcgis.com/VS6HdKS0VfIhv8Ct/arcgis/rest/services/COVID19_Indonesia_per_Provinsi/FeatureServer/0/query?f=json&where=(Kasus_Posi%20%3C%3E%200)%20AND%20(Provinsi%20%3C%3E%20%27Indonesia%27)&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=Kasus_Posi%20desc&resultOffset=0&resultRecordCount=34&resultType=standard&cacheHint=true";
-    private String linkTest = "http://echo.jsontest.com/insert-key-here/insert-value-here/key/value";
-    private JSONObject responses;
-    private boolean isFinishFetch = false;
-
-    private void dataResponse(String url){
-
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
-                Request.Method.GET,
-                url,
-                null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        isFinishFetch = true;
-                        responses = response;
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.e("Error Volley: ", error.toString());
-                    }
-                }
-        );
-
-        requestQueue.add(jsonObjectRequest);
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        dataResponse(linkPerProvinsi);
-        if(isFinishFetch){
-            Log.d("Resp", String.valueOf(responses));
-        }
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment = null;
+
+            switch (item.getItemId()){
+                case R.id.nav_home:
+                    fragment = new HomeFragment();
+                    break;
+                case R.id.nav_stats:
+                    fragment = new StatsFragment();
+                    break;
+                case R.id.nav_info:
+                    fragment = new InfoFragment();
+                    break;
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
+
+            return true;
+        }
+    };
 }

9a0767ad1a9169bff6ac3ff37b4a3d7d2aaca9b0
ayub <yuanaayubs@gmail.com>
2020-06-03 16:59:11 +0700

    padding news and loading

--- app/src/main/java/com/zub/covid_19/NewsHolder.java
+++ app/src/main/java/com/zub/covid_19/NewsHolder.java
@@ -7,7 +7,7 @@ import retrofit2.http.GET;
 
 public interface NewsHolder {
 
-    @GET("v2/top-headlines?country=id&q=covid&apiKey=699fa5b4ab4d49aab02a36bc88eb6cde")
+    @GET("v2/top-headlines?country=id&q=corona&apiKey=699fa5b4ab4d49aab02a36bc88eb6cde")
     Call<News> getNews();
 
 }
--- app/src/main/java/com/zub/covid_19/SpacesItemDecoration.java
+++ app/src/main/java/com/zub/covid_19/SpacesItemDecoration.java
@@ -1,20 +1,28 @@
 package com.zub.covid_19;
 
 import android.graphics.Rect;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
 
 import androidx.recyclerview.widget.RecyclerView;
 
 public class SpacesItemDecoration extends RecyclerView.ItemDecoration {
+    private static final String TAG = "SpacesItemDecoration";
+
     private final int mSpace;
     public SpacesItemDecoration(int space) {
         this.mSpace = space;
     }
+
     @Override
     public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
-        outRect.right = mSpace;
+        int dp = Math.round(mSpace * (view.getResources().getDisplayMetrics().xdpi / DisplayMetrics.DENSITY_DEFAULT));
+        Log.d(TAG, "getItemOffsets: "+dp);
+        outRect.right = dp;
 
         if (parent.getChildAdapterPosition(view) == 0)
-            outRect.left = mSpace;
+            outRect.left = dp;
     }
+
 }
